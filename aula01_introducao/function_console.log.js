b0e95c6674ee30804a376b767f4a853a4c433389
Gabriel Vinícius <97963837+gabrielgvss@users.noreply.github.com>
2022-06-21 23:34:39 -0300

    Add files via upload

---
+++ aula01_introducao/function_console.log.js
@@ -0,0 +1,6 @@
+var myName = "Gabriel";
+var myAge = 18;
+var myLocale = "Brasil";
+
+console.log("Hello World!");
+console.log("Meu nome é",myName,", eu tenho",myAge,"anos e sou um estudante de programação do ",myLocale,".");
